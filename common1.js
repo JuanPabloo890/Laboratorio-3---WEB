8af65ec29dc25ea901bfec3930792c2b1e57befe
Juan Falconi <119060037+JuanPabloo890@users.noreply.github.com>
2023-11-28 01:20:06 -0500

    Delete common1.js

--- common1.js
+++
@@ -1,8 +0,0 @@
-const Games = () => {
-    const games = ['Super Mario Bros', 'The Legend of Zelda', 'Minecraft', 'Final Fantasy', 'Grand Theft Auto'];
-    console.log(games)
-};
-  
-  module.exports = {
-    Games
-  };
